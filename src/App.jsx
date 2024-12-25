6bab5e57f9828e4b3ffe564d0ac88b7e90c9d0f0
madhanv22 <madhankumarv40@gmail.com>
2024-12-25 11:40:47 +0530

    route path added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Search from './pages/Search';
 import History from './pages/History';
+import Navbar from './components/Navbar';
 
 const App = () => (
   <div>
     <Routes>
-      <Route path="/" element={<Dashboard />} />  
+      <Route path="/" element={<Dashboard />} />
       <Route path="/github-dashboard" element={<Dashboard />} />
-      <Route path="/search" element={<><Search /></>} />
-      <Route path="/history" element={<><History /></>} />
-      <Route path="*" element={<div>404 Page Not Found</div>} />
+      <Route path="/search" element={<><Navbar /><Search /></>} />
+      <Route path="/history" element={<><Navbar /><History /></>} />
     </Routes>
   </div>
 );
